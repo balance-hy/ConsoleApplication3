2cf67e6864f52c000f5158f4d60b879e684a740f
Balance <1835675603@qq.com>
2023-07-22 23:16:15 +0800

    添加项目文件。

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,29 @@
+﻿#include<stdio.h>
+#include <stdlib.h>
+int main() {
+	
+	int rows, cols, i, j;
+	scanf("%d %d", &rows, &cols);
+	int** intervals = (int**)malloc(rows * sizeof(int*));
+	for (i = 0; i < rows; i++) {
+		intervals[i] = (int*)malloc(cols * sizeof(int));
+	}
+	for (i = 0; i < rows; i++) {
+		for (j = 0; j < cols; j++) {
+			scanf("%d", &intervals[i][j]);
+		}
+	}
+	for (i = 0; i < rows; i++) {
+		for (j = 0; j < cols; j++) {
+			printf("%d ", intervals[i][j]);
+		}
+		printf("\n");
+	}
+	for (i = 0; i < rows; i++) {
+		free(intervals[i]);
+	}
+	free(intervals);
+	return 0;
+
+}
+
