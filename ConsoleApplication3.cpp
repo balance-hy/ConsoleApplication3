42889599e6c9c70c36f76deee3e4d157da72523a
Balance <1835675603@qq.com>
2023-09-04 16:12:48 +0800

    changes

--- ConsoleApplication3.cpp
+++ ConsoleApplication3.cpp
@@ -1,27 +1,11 @@
 ﻿#include<stdio.h>
 #include <stdlib.h>
+#include "FindAlgorithm.h"
 int main() {
-	int a[6] = { 0,1,2,3,4,5 },i,n;
+	int a[6] = { 0,7,9,11,15,16 },i,n;
 	scanf("%d", &n);
-	int low = 0;
-	int high = 5;
-	int mid = (low + high) / 2;
-	while(low<=high){
-		int mid = (low + high) / 2;
-		if (a[mid] == n) {
-			printf("find it\n");
-			printf("%d\n",mid);
-			break;
-		}
-		else if (a[mid]<n) {
-			low = mid + 1;
-		}
-		else {
-			high = mid - 1;
-		}
-	}
-	if (low > high) {
-		printf("wrong!\n");
+	if (binarySearch(a, 6,n) != -1) {
+		printf("%d\n", binarySearch(a, 6, n));
 	}
 	return 0;
 }
